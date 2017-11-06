11c58dae5f3432352b5c691bc1fa07cf7e1f0fef
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-11-06 16:32:32 +0100

    Push du contact

---
+++ Views/contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Contacts</title>
+    <link rel="stylesheet" style="text/css" href="../assets/css/contact.css">
+  </head>
+  <body>
+    <div class="main">
+      <h1>Le magasin</h1>
+      <p>Le magasin est ouvert de 10h à 12h30 ou sur rendez-vous.</p>
+      <h2>Contact</h2>
+      <div class="info_bulle">
+        <div class="mail">dmotte@skynet.be</div>
+        <p>+32 495 20.59.34<br>
+          Rue Saint Georges 35,<br>
+          1400 Nivelles, Belgique.</p>
+      </div>
+        <div class="facebook">
+          Pour plus d'infos, suivez nous sur notre page facebook !
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2532.5890241127404!2d4.324954415736733!3d50.597591579495955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c232d3694f6c59%3A0xe7333aff0bd54fec!2sRue+Saint-Georges+35%2C+1400+Nivelles!5e0!3m2!1sfr!2sbe!4v1509978641733" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <p>Lieux de départ
+            <input type="text" name="from">
+            <input type="submit" name="" value="Itinéraire">
+          </p>
+    </div>
+  </body>
+</html>

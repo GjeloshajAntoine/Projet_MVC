5addf4f3a0344fdb9eb2058dc714c3344e6fe0c6
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-11-07 16:32:23 +0100

    Contact+CSS

--- Views/contact.php
+++ Views/contact.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Contacts</title>
     <link rel="stylesheet" style="text/css" href="../assets/css/contact.css">
+    <link href="https://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet">
   </head>
   <body>
     <div class="main">
@@ -20,7 +21,7 @@
       </div>
 
       <div class="facebook">
-        <p>Pour plus d'infos, suivez nous sur notre page <a href="https://www.facebook.com/antiquites.decoration" target="_blank" rel="noopener">facebook !</a></p>
+        <p>Pour plus d'infos, suivez nous sur notre page <a class="face" href="https://www.facebook.com/antiquites.decoration" target="_blank" rel="noopener">facebook !</a></p>
       </div>
       <br>
         <iframe class='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2532.5890241127404!2d4.324954415736733!3d50.597591579495955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c232d3694f6c59%3A0xe7333aff0bd54fec!2sRue+Saint-Georges+35%2C+1400+Nivelles!5e0!3m2!1sfr!2sbe!4v1509978641733" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>

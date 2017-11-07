2e3a8be29e9d59ee9962adbe1c04702b5cf32d26
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-07 16:34:51 +0100

    f

--- Views/onepage.php
+++ Views/onepage.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
+
     <link rel="stylesheet" href="onepage.css">
-  </head>
-  <body>
-    <!--
-    <div class="header lang-menu">
-      <div class="langues-container">
-        <div class="langues-buttons">
-          English
-        </div>
-        <div class="langues-buttons">
-          Français
-        </div>
-      </div>
-    </div>-->
+
     <div class="welcome-section">
       <!--  <img src="https://didiermotte.be/wp-content/themes/didiermotte-2.1/library/images/touchwood-logo-sans-slogan-nobelBold-black.png" alt="">-->
         <h1>Didier  Motte</h2>
@@ -31,9 +15,3 @@
       <h1>"L'ennui naquit un jour de l'uniformité"</h1>
       <h3>– Antoine Houdar de la Motte</h3>
     </div>
-  <!--  <div class="footer">
-      <p>© 2017 Didier Motte  +32 495 20 59 34 - </p>*
-      <p>Décorateur, Antiquaire, agencement d'intérieurs, Belgique</p>
-    </div>-->
-  </body>
-</html>

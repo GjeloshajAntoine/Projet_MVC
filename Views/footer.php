93545d836a1c56f8d47869a6857f81b09410acca
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-08 14:55:15 +0100

    body footer coupé décalé

--- Views/footer.php
+++ Views/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/index.css">
-  <title>Project_mvc</title>
-</head>
-<body>
-
-
   <footer>
     <p>© 2017 Didier Motte +32 495 20 59 34 - dmotte@skynet.be - Access map</p>
 
@@ -22,4 +10,3 @@
   </footer>
 
 </body>
-</html>
